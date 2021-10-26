8372b63802ed0d10ca3aeefeb2c2425eec5101e3
Likit Preeyanon <likit.pre@mahidol.edu>
2021-10-26 22:30:34 +0700

    added firestore and storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCy-GU37EF62Xaajdb1nAw4ImteoZncgns",
     authDomain: "uberhealth-c1541.firebaseapp.com",
@@ -14,3 +12,13 @@ const firebaseConfig = {
     appId: "1:115846268061:web:ab4ddd1c3e585efa298074",
     measurementId: "G-NFBWZFHPZB"
 };
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export {
+    storage,
+    db,
+}
