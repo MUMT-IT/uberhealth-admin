59737baa67e84944282c18e7da32a4b5c4a08aba
Likit Preeyanon <likit.pre@mahidol.edu>
2021-10-26 22:28:05 +0700

    initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCy-GU37EF62Xaajdb1nAw4ImteoZncgns",
+    authDomain: "uberhealth-c1541.firebaseapp.com",
+    projectId: "uberhealth-c1541",
+    storageBucket: "uberhealth-c1541.appspot.com",
+    messagingSenderId: "115846268061",
+    appId: "1:115846268061:web:ab4ddd1c3e585efa298074",
+    measurementId: "G-NFBWZFHPZB"
+};
