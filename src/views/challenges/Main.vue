7ba1b02f0c5bfe64b59f1705ff662b9324514396
Likit Preeyanon <likit.pre@mahidol.edu>
2021-10-27 17:14:23 +0700

    display a list of challenges on the menu

--- src/views/challenges/Main.vue
+++ src/views/challenges/Main.vue
@@ -2,12 +2,18 @@
 <div>
   <section class="section">
     <div class="columns">
-      <div class="column is-one-third is-offset-4">
+      <div class="column is-two-thirds is-offset-2">
         <h1 class="title has-text-centered">Challenges</h1>
         <nav class="panel">
           <p class="panel-heading">
             Menu
           </p>
+          <a class="panel-block" v-for="challenge in challenges" :key="challenge.id">
+            <span class="panel-icon">
+              <i class="fas fa-chevron-right" aria-hidden="true"></i>
+            </span>
+            {{ challenge.title }}
+          </a>
           <router-link class="panel-block" :to="{ name: 'ChallengeForm' }">
             <span class="panel-icon">
               <i class="fas fa-plus-circle" aria-hidden="true"></i>
@@ -22,8 +28,32 @@
 </template>
 
 <script>
+import { db, auth } from "../../firebase"
+import {collection, query, where, getDocs} from "firebase/firestore";
+
 export default {
-  name: "Main"
+  name: "Main",
+  data () {
+    return {
+      challenges: []
+    }
+  },
+  methods: {
+    async loadData() {
+      const self = this
+      const q = query(collection(db, "challenges"),
+          where("creator", "==", auth.currentUser.email));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach(doc => {
+        let data = doc.data()
+        data.id = doc.id
+        self.challenges.push(data)
+      });
+    }
+  },
+  mounted() {
+    this.loadData()
+  }
 }
 </script>
 
