784a08c504f6a015c40d257997850ef195fa03ec
Likit Preeyanon <likit.pre@mahidol.edu>
2021-10-27 17:13:01 +0700

    updated the look of the form

--- src/components/challenges/WalkingForm.vue
+++ src/components/challenges/WalkingForm.vue
@@ -1,9 +1,18 @@
 <template>
-<div class="modal-card">
-  <b-field label="จำนวนก้าว">
-    <b-numberinput :value="steps" @input="updateSteps($event)"></b-numberinput>
-  </b-field>
-  <b-button @click="close">Add</b-button>
+<div class="modal-card" style="width: auto">
+  <header class="modal-card-head">
+    <p class="modal-card-title">Walking Activity</p>
+  </header>
+  <section class="modal-card-body">
+    <b-field label="จำนวนก้าว">
+      <b-numberinput :value="steps" @input="updateSteps($event)"></b-numberinput>
+    </b-field>
+  </section>
+  <footer class="modal-card-foot">
+    <div class="buttons is-centered">
+      <b-button @click="close" class="is-primary">Add</b-button>
+    </div>
+  </footer>
 </div>
 </template>
 
