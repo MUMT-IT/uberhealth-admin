2a111bb9978ae8afeb9b756a96ecc1bce045f5da
Likit Preeyanon <likit.pre@mahidol.edu>
2021-10-27 17:13:46 +0700

    added a method that saves data

--- src/views/challenges/Form.vue
+++ src/views/challenges/Form.vue
@@ -3,42 +3,43 @@
   <section class="section">
     <div class="container">
       <div class="columns">
-        <div class="column is-two-thirds is-offset-3 box">
-          <b-field label="ชื่อโครงการ">
+        <div class="column is-two-thirds is-offset-2 box">
+          <h1 class="title has-text-centered">New Challenge</h1>
+          <b-field label="ชื่อโครงการ" type="is-danger">
             <b-input type="text" v-model="challenge.title"></b-input>
           </b-field>
-          <b-field label="ระยะเวลา">
+          <b-field label="ระยะเวลา" type="is-danger">
             <b-datepicker v-model="challenge.dates" range></b-datepicker>
+          </b-field>
+          <b-field label="กิจกรรม">
+            <ul v-for="activity in challenge.activities" :key="activity.type">
+              <li>{{ activity.type }}</li>
+            </ul>
           </b-field>
           <div class="buttons is-centered">
             <b-button class="is-primary" @click="isComponentWalkingModalActive=true">
               Add Walking Activity
             </b-button>
-            <b-button class="is-success">
-              Submit
+            <b-button @click="saveData" class="is-success">
+              Save
             </b-button>
           </div>
         </div>
       </div>
-      <pre>
-        {{ challenge }}
-      </pre>
-      <pre>
-        {{ walkingFormProps }}
-      </pre>
     </div>
     <b-modal
         v-model="isComponentWalkingModalActive"
-        :has-modal-card="true"
+        has-modal-card
         trap-focus
         :destroy-on-hide="false"
         aria-role="dialog"
-        aria-label="Example Modal"
+        aria-label="Walking Activity Form"
         aria-modal>
       <template #default="props">
         <walking-form v-bind="walkingFormProps"
                       @closing="addWalkingActivity"
-                      v-on:update-steps="updateSteps" @close="props.close"></walking-form>
+                      v-on:update-steps="updateSteps" @close="props.close">
+        </walking-form>
       </template>
     </b-modal>
   </section>
@@ -47,7 +48,9 @@
 
 <script>
 import WalkingForm from "../../components/challenges/WalkingForm";
-import {auth} from "../../firebase";
+import { auth, db } from "../../firebase";
+import { collection, addDoc } from "firebase/firestore"
+
 export default {
   name: "Form",
   components: {
@@ -74,10 +77,30 @@ export default {
     },
     addWalkingActivity () {
       this.challenge.activities.push({
-        activity: 'walking',
+        type: 'walking',
         steps: this.walkingFormProps.steps
       })
       this.walkingFormProps.steps = 0
+    },
+    async saveData() {
+      const self = this
+      if (self.challenge.title && self.challenge.dates) {
+        const docRef = await addDoc(collection(db, "challenges"), self.challenge)
+        if (docRef.id) {
+          self.$buefy.toast.open({
+            message: "Saved successfully",
+            type: "is-success",
+            position: "is-bottom"
+          })
+          self.$router.push({ name: "ChallengeMain"})
+        }
+      } else {
+        self.$buefy.toast.open({
+          message: "Please fill out all required fields",
+          type: "is-danger",
+          position: "is-bottom"
+        })
+      }
     }
   }
 }
